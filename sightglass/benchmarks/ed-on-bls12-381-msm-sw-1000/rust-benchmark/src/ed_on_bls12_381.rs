f48aa5cb6d9b32ae53459747c550ada90e3903a8
Achim Schneider <achim@parity.io>
2023-03-07 16:15:19 +0100

    fix imports

--- sightglass/benchmarks/ed-on-bls12-381-msm-sw-1000/rust-benchmark/src/ed_on_bls12_381.rs
+++ sightglass/benchmarks/ed-on-bls12-381-msm-sw-1000/rust-benchmark/src/ed_on_bls12_381.rs
@@ -2,7 +2,7 @@ use ark_std::{io::Error, vec::Vec};
 use ark_ec::{pairing::Pairing, AffineRepr, CurveConfig, Group};
 
 pub fn do_msm_sw(
-	bases: &[sp_ark_models::short_weierstrass::Affine<ark_ed_on_bls12_381::SWConfig>],
+	bases: &[ark_e::short_weierstrass::Affine<ark_ed_on_bls12_381::SWConfig>],
 	scalars: &[<ark_ed_on_bls12_381::SWConfig as ark_ec::CurveConfig>::ScalarField],
 ) -> Result<(), Error> {
 	let _out = <ark_ed_on_bls12_381::EdwardsConfig as ark_ec::short_weierstrass::SWCurveConfig>::msm(&bases[..], &scalars[..]);
@@ -29,7 +29,7 @@ pub fn do_mul_affine_sw() -> Result<(), Error> {
 
 pub fn do_mul_affine_te() -> Result<(), Error> {
 	let _out =
-		<ark_ed_on_bls12_381::EdwardsConfig as sp_ark_models::short_weierstrass::SWCurveConfig>::mul_affine(
+		<ark_ed_on_bls12_381::EdwardsConfig as ark_e::short_weierstrass::SWCurveConfig>::mul_affine(
 			&ark_ed_on_bls12_381::SWAffine::generator(),
 			&[2u64],
 		);
@@ -38,7 +38,7 @@ pub fn do_mul_affine_te() -> Result<(), Error> {
 
 pub fn do_mul_projective_sw() -> Result<(), Error> {
 	let _out =
-		<ark_ed_on_bls12_381::EdwardsConfig as sp_ark_models::short_weierstrass::SWCurveConfig>::mul_projective(
+		<ark_ed_on_bls12_381::EdwardsConfig as ark_e::short_weierstrass::SWCurveConfig>::mul_projective(
 			&ark_ed_on_bls12_381::SWProjective::generator(),
 			&[2u64],
 		);
