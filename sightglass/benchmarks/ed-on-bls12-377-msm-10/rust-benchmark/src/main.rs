f8909f93eefd4aea3cccba976042c4b3265260e7
Achim Schneider <achim@parity.io>
2023-03-07 17:05:43 +0100

    fix imports

--- sightglass/benchmarks/ed-on-bls12-377-msm-10/rust-benchmark/src/main.rs
+++ sightglass/benchmarks/ed-on-bls12-377-msm-10/rust-benchmark/src/main.rs
@@ -8,7 +8,7 @@ use ark_ec::CurveGroup;
 use ed_on_bls12_377::do_msm;
 
 fn main() {
-    let (bases, scalars) = generate_arguments::<sark_ed_on_bls12_377::EdwardsProjective>(10);
+    let (bases, scalars) = generate_arguments::<ark_ed_on_bls12_377::EdwardsProjective>(10);
     let bases = bases.iter().map(|base| base.into_affine()).collect::<Vec<_>>();
     bench::start();
     let result = do_msm(&bases[..], &scalars[..]);
