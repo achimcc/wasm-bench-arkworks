9012db52c15d8d90e2366e3bfe232bd0918fcbd0
Achim Schneider <achim@parity.io>
2023-03-07 18:06:29 +0100

    fix imports

--- sightglass/benchmarks/bls12-381-msm-g2-1000/rust-benchmark/src/main.rs
+++ sightglass/benchmarks/bls12-381-msm-g2-1000/rust-benchmark/src/main.rs
@@ -8,7 +8,7 @@ use ark_ec::CurveGroup;
 use bls12_381::do_msm_g2;
 
 fn main() {
-    let (bases, scalars) = generate_arguments::<ark_ec::short_weierstrass::Projective<ark_bls12_381::g1::Config>>(10);
+    let (bases, scalars) = generate_arguments::<ark_ec::short_weierstrass::Projective<ark_bls12_381::g2::Config>>(1000);
     let bases = bases.iter().map(|base| base.into_affine()).collect::<Vec<_>>();
     bench::start();
     let result = do_msm_g2(&bases[..], &scalars[..]);
