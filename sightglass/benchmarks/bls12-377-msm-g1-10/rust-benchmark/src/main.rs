6b11425d2094936cd2881a77b1701960aee462bb
Achim Schneider <achim@parity.io>
2023-03-07 17:29:48 +0100

    fix imports

--- sightglass/benchmarks/bls12-377-msm-g1-10/rust-benchmark/src/main.rs
+++ sightglass/benchmarks/bls12-377-msm-g1-10/rust-benchmark/src/main.rs
@@ -11,7 +11,7 @@ fn main() {
     let (bases, scalars) = generate_arguments::<ark_ec::short_weierstrass::Projective<ark_bls12_377::g1::Config>>(10);
     let bases = bases.iter().map(|base| base.into_affine()).collect::<Vec<_>>();
     bench::start();
-    let result = do_msm_g1(10);
+    let result = do_msm_g1(&bases[..], &scalars[..]);
     bench::end();
     assert_eq!(result.unwrap(), ());
 }
