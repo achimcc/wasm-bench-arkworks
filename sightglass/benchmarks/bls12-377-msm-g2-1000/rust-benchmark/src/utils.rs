e9740644320383e62b2ef07d5e275be76b41c68b
Achim Schneider <achim@parity.io>
2023-03-07 09:52:20 +0100

    fix imports

--- sightglass/benchmarks/bls12-377-msm-g2-1000/rust-benchmark/src/utils.rs
+++ sightglass/benchmarks/bls12-377-msm-g2-1000/rust-benchmark/src/utils.rs
@@ -1,11 +1,10 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use ark_serialize::{CanonicalSerialize, Compress};
 use ark_std::{io::Cursor, test_rng, vec, vec::Vec, UniformRand};
 
 pub fn generate_arguments<Group: ark_ec::VariableBaseMSM>(
 	size: u32,
-) -> (Vec<Group>, Vec<<Group as Group>::ScalarField>) {
+) -> (Vec<Group>, Vec<Group::ScalarField>) {
 	let rng = &mut test_rng();
 	let scalars = (0..size).map(|_| Group::ScalarField::rand(rng)).collect::<Vec<_>>();
 	let bases = (0..size).map(|_| Group::rand(rng)).collect::<Vec<_>>();
