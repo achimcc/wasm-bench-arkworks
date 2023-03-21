0ec6924bc98d568fcf018a12d64ace532723fa5a
Achim Schneider <achim@parity.io>
2023-03-21 18:59:16 +0100

    fix types

--- sightglass/benchmarks/ed-on-bls12-381-mul-projective-te/rust-benchmark/src/ed_on_bls12_381.rs
+++ sightglass/benchmarks/ed-on-bls12-381-mul-projective-te/rust-benchmark/src/ed_on_bls12_381.rs
@@ -10,7 +10,7 @@ pub fn do_msm_sw(
 }
 
 pub fn do_msm_te(
-	bases: &[EdwardsAffineOptimized],
+	bases: &[ark_ec::twisted_edwards::Affine<ark_ed_on_bls12_381::SWConfig>],
 	scalars: &[<ark_ed_on_bls12_381::EdwardsConfig as ark_ec::CurveConfig>::ScalarField],
 ) -> Result<(), Error> {
 	let _out = <ark_ed_on_bls12_381::JubjubConfig as ark_ec::twisted_edwards::TECurveConfig>::msm(
@@ -24,7 +24,7 @@ pub fn do_mul_affine_sw(base: &ark_ed_on_bls12_381::SWAffine, scalar: &[u64]) ->
 	Ok(())
 }
 
-pub fn do_mul_affine_te(base: &EdwardsAffineOptimized, scalar: &[u64]) -> Result<(), Error> {
+pub fn do_mul_affine_te(base: &ark_ec::twisted_edwards::Affine<ark_ed_on_bls12_381::SWConfig>, scalar: &[u64]) -> Result<(), Error> {
 	let _out =
 		<ark_ed_on_bls12_381::EdwardsConfig as ark_ec::twisted_edwards::TECurveConfig>::mul_affine(
 			base,
