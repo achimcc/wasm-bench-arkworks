bc038409b41c120dbbbec4ee9587c537a8dab27d
Achim Schneider <achim@parity.io>
2023-03-21 18:13:15 +0100

    fix args

--- sightglass/benchmarks/bw6-761-mul-affine-g2/rust-benchmark/src/main.rs
+++ sightglass/benchmarks/bw6-761-mul-affine-g2/rust-benchmark/src/main.rs
@@ -10,7 +10,7 @@ use bw6_761::do_mul_affine_g2;
 fn main() {
     let (base, scalar) = generate_scalar_args::<ark_ec::short_weierstrass::Affine<ark_bw6_761::g2::Config>>();
     bench::start();
-    let result = do_mul_affine_g2(&base, scalar);
+    let result = do_mul_affine_g2(&base, &[scalar]);
     bench::end();
     assert_eq!(result.unwrap(), ());
 }
--- sightglass/benchmarks/ed-on-bls12-381-mul-projective-te/rust-benchmark/src/main.rs
+++ sightglass/benchmarks/ed-on-bls12-381-mul-projective-te/rust-benchmark/src/main.rs
@@ -10,7 +10,7 @@ use ed_on_bls12_381::do_mul_projective_te;
 fn main() {
     let (base, scalar) = generate_scalar_args::<ark_ed_on_bls12_381::EdwardsProjective>();
     bench::start();
-    let result = do_mul_projective_te(&base, scalar);
+    let result = do_mul_projective_te(&base, &[scalar]);
     bench::end();
     assert_eq!(result.unwrap(), ());
 }
