2e374088ec584e266186b6146c2a3503428827c3
Achim Schneider <achim@parity.io>
2023-03-21 16:36:15 +0100

    fix imports

--- sightglass/benchmarks/bls12-377-mul-projective-g2/rust-benchmark/src/bls12_377.rs
+++ sightglass/benchmarks/bls12-377-mul-projective-g2/rust-benchmark/src/bls12_377.rs
@@ -1,57 +1,8 @@
 use ark_std::{io::Error, vec::Vec};
-pub use ark_bls12_377::{
-	Bls12_377 as Bls12_377_Host, G1Affine as G1AffineOptimized_Host,
-	G1Projective as G1ProjectiveOptimized_Host, G2Affine as G2AffineOptimized_Host,
-	G2Projective as G2ProjectiveOptimized_Host, HostFunctions as Bls12_377HostFunctions,
-};
 pub use ark_ec::{pairing::Pairing, short_weierstrass::SWCurveConfig, AffineRepr, Group};
-use arkworks::PairingError;
-
-pub struct HostBls12_377 {}
-
-impl Bls12_377HostFunctions for HostBls12_377 {
-	fn bls12_377_multi_miller_loop(
-		a: Vec<Vec<u8>>,
-		b: Vec<Vec<u8>>,
-	) -> Result<Vec<u8>, PairingError> {
-		sp_io::elliptic_curves::bls12_377_multi_miller_loop(a, b)
-	}
-	fn bls12_377_final_exponentiation(f12: Vec<u8>) -> Result<Vec<u8>, PairingError> {
-		sp_io::elliptic_curves::bls12_377_final_exponentiation(f12)
-	}
-	fn bls12_377_msm_g1(bases: Vec<Vec<u8>>, bigints: Vec<Vec<u8>>) -> Vec<u8> {
-		sp_io::elliptic_curves::bls12_377_msm_g1(bases, bigints)
-	}
-	fn bls12_377_msm_g2(bases: Vec<Vec<u8>>, bigints: Vec<Vec<u8>>) -> Vec<u8> {
-		sp_io::elliptic_curves::bls12_377_msm_g2(bases, bigints)
-	}
-	fn bls12_377_mul_projective_g1(base: Vec<u8>, scalar: Vec<u8>) -> Vec<u8> {
-		sp_io::elliptic_curves::bls12_377_mul_projective_g1(base, scalar)
-	}
-	fn bls12_377_mul_affine_g1(base: Vec<u8>, scalar: Vec<u8>) -> Vec<u8> {
-		sp_io::elliptic_curves::bls12_377_mul_affine_g1(base, scalar)
-	}
-	fn bls12_377_mul_projective_g2(base: Vec<u8>, scalar: Vec<u8>) -> Vec<u8> {
-		sp_io::elliptic_curves::bls12_377_mul_projective_g2(base, scalar)
-	}
-	fn bls12_377_mul_affine_g2(base: Vec<u8>, scalar: Vec<u8>) -> Vec<u8> {
-		sp_io::elliptic_curves::bls12_377_mul_affine_g2(base, scalar)
-	}
-}
-
-pub type Bls12_377Optimized = Bls12_377_Host<HostBls12_377>;
-pub type G1AffineOptimized = G1AffineOptimized_Host<HostBls12_377>;
-pub type G2AffineOptimized = G2AffineOptimized_Host<HostBls12_377>;
-pub type G1ProjectiveOptimized = G1ProjectiveOptimized_Host<HostBls12_377>;
-pub type G2ProjectiveOptimized = G2ProjectiveOptimized_Host<HostBls12_377>;
 
 pub fn do_pairing(a: ark_bls12_377::G1Affine, b: ark_bls12_377::G2Affine) -> Result<(), Error> {
 	let _out = ark_bls12_377::Bls12_377::multi_pairing([a], [b]);
-	Ok(())
-}
-
-pub fn do_pairing_optimized(a: G1AffineOptimized, b: G2AffineOptimized) -> Result<(), Error> {
-	let _out = Bls12_377Optimized::multi_pairing([a], [b]);
 	Ok(())
 }
 
@@ -63,34 +14,11 @@ pub fn do_msm_g1(
 	Ok(())
 }
 
-pub fn do_msm_g1_optimized(
-	bases: &[ark_ec::short_weierstrass::Affine<
-		ark_bls12_377::g1::Config<HostBls12_377>,
-	>],
-	scalars: &[<ark_bls12_377::g1::Config<HostBls12_377> as ark_ec::CurveConfig>::ScalarField],
-) -> Result<(), Error> {
-	let _out = <ark_bls12_377::g1::Config<HostBls12_377> as ark_ec::short_weierstrass::SWCurveConfig>::msm(
-		bases,
-		scalars,
-	);
-	Ok(())
-}
-
 pub fn do_msm_g2(
 	bases: &[ark_ec::short_weierstrass::Affine<ark_bls12_377::g2::Config>],
 	scalars: &[<ark_bls12_377::g2::Config as ark_ec::CurveConfig>::ScalarField],
 ) -> Result<(), Error> {
 	let _out = <ark_bls12_377::g2::Config as SWCurveConfig>::msm(bases, scalars);
-	Ok(())
-}
-
-pub fn do_msm_g2_optimized(
-	bases: &[ark_ec::short_weierstrass::Affine<
-		ark_bls12_377::g2::Config<HostBls12_377>,
-	>],
-	scalars: &[<ark_bls12_377::g2::Config<HostBls12_377> as ark_ec::CurveConfig>::ScalarField],
-) -> Result<(), Error> {
-	let _out = <ark_bls12_377::g2::Config<HostBls12_377> as SWCurveConfig>::msm(bases, scalars);
 	Ok(())
 }
 
@@ -100,17 +28,6 @@ pub fn do_mul_affine_g1(
 ) -> Result<(), Error> {
 	let _out = <ark_bls12_377::g1::Config as ark_ec::short_weierstrass::SWCurveConfig>::mul_affine(
 		base, scalar,
-	);
-	Ok(())
-}
-
-pub fn do_mul_affine_g1_optimized(
-	base: &ark_ec::short_weierstrass::Affine<ark_bls12_377::g1::Config<HostBls12_377>>,
-	scalar: &[u64],
-) -> Result<(), Error> {
-	let _out = <ark_bls12_377::g1::Config<HostBls12_377> as ark_ec::short_weierstrass::SWCurveConfig>::mul_affine(
-		base,
-		scalar,
 	);
 	Ok(())
 }
@@ -126,36 +43,12 @@ pub fn do_mul_projective_g1(
 	Ok(())
 }
 
-pub fn do_mul_projective_g1_optimized(
-	base: &ark_ec::short_weierstrass::Projective<
-		ark_bls12_377::g1::Config<HostBls12_377>,
-	>,
-	scalar: &[u64],
-) -> Result<(), Error> {
-	let _out = <ark_bls12_377::g1::Config<HostBls12_377> as ark_ec::short_weierstrass::SWCurveConfig>::mul_projective(
-		base,
-		scalar,
-	);
-	Ok(())
-}
-
 pub fn do_mul_affine_g2(
 	base: &ark_ec::short_weierstrass::Affine<ark_bls12_377::g2::Config>,
 	scalar: &[u64],
 ) -> Result<(), Error> {
 	let _out = <ark_bls12_377::g2::Config as ark_ec::short_weierstrass::SWCurveConfig>::mul_affine(
 		base, scalar,
-	);
-	Ok(())
-}
-
-pub fn do_mul_affine_g2_optimized(
-	base: &ark_ec::short_weierstrass::Affine<ark_bls12_377::g2::Config<HostBls12_377>>,
-	scalar: &[u64],
-) -> Result<(), Error> {
-	let _out = <ark_bls12_377::g2::Config<HostBls12_377> as ark_ec::short_weierstrass::SWCurveConfig>::mul_affine(
-		base,
-		scalar,
 	);
 	Ok(())
 }
@@ -168,18 +61,5 @@ pub fn do_mul_projective_g2(
 		<ark_bls12_377::g2::Config as ark_ec::short_weierstrass::SWCurveConfig>::mul_projective(
 			base, scalar,
 		);
-	Ok(())
-}
-
-pub fn do_mul_projective_g2_optimized(
-	base: &ark_ec::short_weierstrass::Projective<
-		ark_bls12_377::g2::Config<HostBls12_377>,
-	>,
-	scalar: &[u64],
-) -> Result<(), Error> {
-	let _out = <ark_bls12_377::g2::Config<HostBls12_377> as ark_ec::short_weierstrass::SWCurveConfig>::mul_projective(
-		base,
-		scalar,
-	);
 	Ok(())
 }
--- sightglass/benchmarks/ed-on-bls12-377-mul-projective/rust-benchmark/src/ed_on_bls12_377.rs
+++ sightglass/benchmarks/ed-on-bls12-377-mul-projective/rust-benchmark/src/ed_on_bls12_377.rs
@@ -1,24 +1,5 @@
 use ark_std::{io::Error, vec::Vec};
-use ark_ed_on_bls12_377::HostFunctions as EdwardBls12_377HostFunctions;
 use ark_ec::TECurveConfig;
-
-pub struct HostEdOnBls12_377 {}
-
-impl EdwardBls12_377HostFunctions for HostEdOnBls12_377 {
-	fn ed_on_bls12_377_msm(bases: Vec<Vec<u8>>, scalars: Vec<Vec<u8>>) -> Vec<u8> {
-		sp_io::elliptic_curves::ed_on_bls12_377_msm(bases, scalars)
-	}
-	fn ed_on_bls12_377_mul_affine(base: Vec<u8>, scalar: Vec<u8>) -> Vec<u8> {
-		sp_io::elliptic_curves::ed_on_bls12_377_mul_affine(base, scalar)
-	}
-	fn ed_on_bls12_377_mul_projective(base: Vec<u8>, scalar: Vec<u8>) -> Vec<u8> {
-		sp_io::elliptic_curves::ed_on_bls12_377_mul_projective(base, scalar)
-	}
-}
-
-pub type EdwardsAffineOptimized =
-	ark_ec::twisted_edwards::Affine<ark_ed_on_bls12_377::EdwardsConfig>;
-pub type EdwardsProjectiveOptimized = ark_ed_on_bls12_377::EdwardsProjective<HostEdOnBls12_377>;
 
 pub fn do_msm(
 	bases: &[ark_ec::twisted_edwards::Affine<ark_ed_on_bls12_377::EdwardsConfig>],
@@ -28,16 +9,6 @@ pub fn do_msm(
 		<ark_ed_on_bls12_377::EdwardsConfig as ark_ec::models::twisted_edwards::TECurveConfig>::msm(
 			bases, scalars,
 		);
-	Ok(())
-}
-
-pub fn do_msm_optimized(
-	bases: &[EdwardsAffineOptimized],
-	scalars: &[<ark_ed_on_bls12_377::EdwardsConfig<HostEdOnBls12_377> as ark_ec::CurveConfig>::ScalarField],
-) -> Result<(), Error> {
-	let _out = <ark_ed_on_bls12_377::EdwardsConfig<HostEdOnBls12_377> as TECurveConfig>::msm(
-		bases, scalars,
-	);
 	Ok(())
 }
 
@@ -53,29 +24,10 @@ pub fn do_mul_affine(
 	Ok(())
 }
 
-pub fn do_mul_affine_optimized(base: &EdwardsAffineOptimized, scalar: &[u64]) -> Result<(), Error> {
-	let _out =
-		<ark_ed_on_bls12_377::EdwardsConfig<HostEdOnBls12_377> as TECurveConfig>::mul_affine(
-			base, scalar,
-		);
-	Ok(())
-}
-
 pub fn do_mul_projective(
 	base: &ark_ed_on_bls12_377::EdwardsProjective,
 	scalar: &[u64],
 ) -> Result<(), Error> {
 	let _out = <ark_ed_on_bls12_377::EdwardsConfig as TECurveConfig>::mul_projective(base, scalar);
-	Ok(())
-}
-
-pub fn do_mul_projective_optimized(
-	base: &EdwardsProjectiveOptimized,
-	scalar: &[u64],
-) -> Result<(), Error> {
-	let _out =
-		<ark_ed_on_bls12_377::EdwardsConfig<HostEdOnBls12_377> as TECurveConfig>::mul_projective(
-			base, scalar,
-		);
 	Ok(())
 }
